165b1366f4876913f4bcd3a93d6b4a86ebf1f29c
MaisUmHugo <149391609+MaisUmHugo@users.noreply.github.com>
2026-03-03 10:01:58 -0300

    feat: Base cenas PauseUI e Controles

--- Scripts/UI/ControlesUI.gd
+++ Scripts/UI/ControlesUI.gd
@@ -1,11 +1,48 @@
-extends Node
+extends Control
 
+@onready var pause_panel: Control = $CenterContainer
+@onready var controles: Control = $ControlesUI
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+func _ready():
+	visible = false
+	process_mode = Node.PROCESS_MODE_WHEN_PAUSED
 
+	pause_panel.visible = true
+	controles.visible = false
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _input(event):
+	if event.is_action_pressed("pause"):
+		if GameManager.estadoAtual == GameManager.EstadoJogo.Jogando:
+			MostrarPause()
+		elif GameManager.estadoAtual == GameManager.EstadoJogo.Pausado:
+			if controles.visible:
+				fechar_controles()
+			else:
+				EsconderPause()
+
+func MostrarPause():
+	visible = true
+	GameManager.PausarJogo()
+	pause_panel.visible = true
+	controles.visible = false
+
+func EsconderPause():
+	visible = false
+	GameManager.RetomarJogo()
+
+func _on_botaoContinuar_pressed():
+	EsconderPause()
+
+func _on_botaoMenu_pressed():
+	GameManager.VoltarMenuPrincipal()
+
+func _on_botaoControles_pressed():
+	abrir_controles()
+
+func abrir_controles():
+	pause_panel.visible = false
+	controles.visible = true
+
+func fechar_controles():
+	controles.visible = false
+	pause_panel.visible = true

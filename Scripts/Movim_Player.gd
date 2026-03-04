e74de451f3bf9a644319ce2b93252570ff900f7e
Gabriel Silva Lourenço <gsl10114a@gmail.com>
2026-03-04 17:11:04 -0300

    Animação player

--- Scripts/Movim_Player.gd
+++ Scripts/Movim_Player.gd
@@ -6,12 +6,12 @@ var forca_externa: Vector2 = Vector2.ZERO
 var esta_na_sombra: bool = false
 var color = null
 var tocha_atual = null
+var ultima_direcao: Vector2 = Vector2.DOWN
 
 @onready var looks: AnimatedSprite2D = get_node_or_null("%looks")
 
 func empurrar(direcao: Vector2, forca: float) -> void:
 	forca_externa += direcao.normalized() * forca
-
 
 func _physics_process(_delta: float) -> void:
 	var direction := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
@@ -21,29 +21,30 @@ func _physics_process(_delta: float) -> void:
 	move_and_slide()
 
 	_atualizar_animacao(direction)
-
-	# Zera só se NÃO estiver na sombra
 	if not esta_na_sombra:
 		forca_externa = Vector2.ZERO
 
 	_empurrar_caixas(direction)
 
-
 func _atualizar_animacao(direction: Vector2) -> void:
 	if looks == null:
 		return
+	if direction != Vector2.ZERO:
+		ultima_direcao = direction
 
-	if direction == Vector2.ZERO:
-		_set_anim("idle")
+	if abs(ultima_direcao.x) > abs(ultima_direcao.y):
+		looks.animation = "andando_lado"
+		looks.flip_h = ultima_direcao.x < 0
+	elif ultima_direcao.y < 0:
+		looks.animation = "Andando_Cima"
 	else:
-		_set_anim("andando")
+		looks.animation = "idle"
 
 func _set_anim(nome: String) -> void:
 	if looks == null:
 		return
 	if looks.animation != nome:
 		looks.play(nome)
-
 
 func _empurrar_caixas(direction: Vector2) -> void:
 	if direction == Vector2.ZERO:
@@ -56,6 +57,5 @@ func _empurrar_caixas(direction: Vector2) -> void:
 		if obj is CaixaSimples:
 			var normal := col.get_normal()
 
-			# Só empurra se estiver pressionando contra a face da caixa
 			if direction.dot(-normal) > 0.7:
 				(obj as CaixaSimples).empurrar(direction)
